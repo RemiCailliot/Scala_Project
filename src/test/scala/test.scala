ca6f36281949402994c0c0ebf2ebdd66670a4de3
RemiCailliot <remi.cailliot@efrei.net>
2022-02-13 18:58:36 +0100

    commit test

--- src/test/scala/test.scala
+++ src/test/scala/test.scala
@@ -1,18 +1,18 @@
-import MainApp._
-import request.Reports
-import org.scalatest._
-import org.scalatest.funsuite.AnyFunSuite
 import model.{Airport, Country, Runway}
-import request.Queries.query1
-import request.Reports.{report1, report2, report3}
+import org.scalatest.funsuite.AnyFunSuite
+import request.Reports.{report1, report2}
 import service.CSV
 
 class TestApp extends AnyFunSuite {
-  test("giveMeHelloString function") {
-    assert(report1(CSV.read("airports.csv",Airport.csvToAirport).lines,CSV.read("countries.csv",Country.csvToCountry).lines)("United States") == )
+  test("report1 assert") {
+    val getr = report1(CSV.read("airports.csv",Airport.csvToAirport).lines,
+      CSV.read("countries.csv",Country.csvToCountry).lines).head.head._2
+    assert(getr == 592)
   }
-
-  test("Reports1 function") {
-    assert(Reports.highest_number_of_airports() == 8)
+  test("report2 assert") {
+    val getr = report2(CSV.read("airports.csv",Airport.csvToAirport).lines,
+      CSV.read("countries.csv",Country.csvToCountry).lines,
+      CSV.read("runways.csv",Runway.csvToRunway).lines).head.head.toMap
+    assert(getr==("ae",List("asp", "mac", "unk", "asphalt", "u")))
   }
 }
